dd8fd7aabb6f103ccd6bee58397ab1583e0e4c67
roberto13-filho <robertofilho.fh@outlook.com>
2025-05-15 20:45:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f2f2f2;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  padding: 20px;
+}
+
+.pokedex {
+  background: white;
+  border-radius: 20px;
+  padding: 30px;
+  box-shadow: 0 0 20px rgba(0,0,0,0.1);
+  display: inline-block;
+  max-width: 300px;
+  width: 100%;
+}
+
+.pokemon-info img {
+  width: 150px;
+  height: 150px;
+}
+
+#search-input {
+  padding: 10px;
+  width: 70%;
+  margin-top: 15px;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+}
+
+button {
+  padding: 10px 15px;
+  margin-top: 15px;
+  border: none;
+  background-color: #ff1c1c;
+  color: white;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #e60000;
+}
